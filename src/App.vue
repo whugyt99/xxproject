9eed2eac054b0fd2a31146316970326205220262
chenyuru <1244535094@qq.com>
2020-06-02 22:41:05 +0800

    feat:导航栏1.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
 <!--    导航栏-->
-    <el-menu :default-active="this.$route.path" class="el-menu-demo" router mode="horizontal"
-             @select="handleSelect" text-color="#fff" active-text-color="#ffd04b" background-color="#545c64">
+    <div style="background-color: #2c3e50;text-align: center;height: 150px">
+      <span style="font-size: xx-large;color: white; margin-top: 10px"><b>xx小分队</b></span>
 
-      <el-menu-item class="el-menu-item" v-for="(item,i) in navList" :key="i" :index="item.name">
-        {{ item.navItem}}
-      </el-menu-item>
+      <el-menu :default-active="this.$route.path" class="el-menu-demo" router mode="horizontal"
+               @select="handleSelect" text-color="#fff" active-text-color="#ffd04b" background-color="#2c3e50">
+        <el-menu-item class="el-menu-item" v-for="(item,i) in navList" :key="i" :index="item.name" style="margin-bottom: 0px">
+          {{ item.navItem}}
+        </el-menu-item>
+      </el-menu>
+    </div>
 
-    </el-menu>
+    <el-image :src="imageSrc" style="width: 100%; height: 250px;background-color: #2c3e50"></el-image>
+
+
 
     <router-view class="menu-right"/>
   </div>
@@ -20,6 +26,7 @@ export default {
 
   data(){
     return {
+      imageSrc: '../static/image/test3.jpeg',
       navList: [
         {name: '/', navItem: '缺陷检测'},
         {name: '/components/HelloWorld', navItem: '模型训练'},
@@ -39,29 +46,24 @@ export default {
 </script>
 
 <style>
-#app {
+#app scoped{
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  /*margin-top: 60px;*/
 }
   .el-menu-demo{
-    height: 60px;
     width: 100%;
-    float: right;
-    background-color: #545c64
+    /*margin-bottom: -100px;*/
+    margin-top: 45px;
+    /*height: 200px;*/
+    /*background-image: url("../static/image/test2.jpeg");*/
+    /*background-repeat: no-repeat;*/
+    /*background-size: cover;*/
+    /*background-color: #545c64*/
   }
-  /*!* 点击出来的下划线进行隐藏 *!*/
-  /*.el-menu-item.is-active {*/
-  /*  border-bottom: none!important;*/
-  /*  color: rgba(0,0,0,0);*/
-  /*}*/
-  /*!* 整体的下划线进行隐藏 *!*/
-  /*.el-menu>.el-menu--horizontal{*/
-  /*  border-bottom: none;*/
-  /*}*/
+
   /* 导航栏的间距等样式 */
   .el-menu-item {
     padding: 0 62px;
