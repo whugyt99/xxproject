fdb396b134691c51b668882a4c450826a2b26724
whugyt99 <1042957000@qq.com>
2020-07-11 23:13:19 +0800

    feat:分类检测上传图片携带参数 style:el-tabs颜色 feat:选择文件名，判断是否可以训练

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div style="margin-top: 20px">
     <el-row style="margin-left: 50px">
       <el-col :span="6" v-for="(item,i) in defectTypes" :key="i" :offset="1">
-        <div style="cursor:pointer" @click="insurance(item.name)">
+        <div style="cursor:pointer" @click="insurance(item.name, item.value)">
           <el-card style="text-align: center;height: 340px;margin-top: 40px"
                    :body-style="{padding: '30px'}" class="el-card" shadow="hover">
 
@@ -41,27 +41,29 @@
       return {
         fit: 'fill',
         id: '',
+  value:'',
         defectTypes: [
-          {imgSrc: '../static/image/缺陷类型/崩边.jpg', name: '崩边', details: '合格标准：\n≦0.02mm²,允许2个，\n或≦0.03mm²,允许1个'},
-          {imgSrc: '../static/image/缺陷类型/亮点.jpg', name: '亮点', details: '合格标准：\n≦0.01mm²,允许1个'},
-          {imgSrc: '../static/image/缺陷类型/内污.jpg', name: '内污', details: '合格标准：\n白色:≦0.02mm²,允许2个,间距大于 5mm.\n黑色:≦0.05mm².'},
-          {imgSrc: '../static/image/缺陷类型/缺墨.jpg', name: '缺墨', details: '合格标准：\n接收孔/菜单孔缺墨，透光：高度≦0.03mm,点状按≦0.03mm²,允许1个\n边缘缺墨：≦0.03mm²，允许一个'},
-          {imgSrc: '../static/image/缺陷类型/视划.jpg', name: '视窗区划伤', details: '合格标准：\n长度<10mm,允许5条，不可交叉，明显度依签样；\n不可有视窗区反面垂直于短边划伤\n'},
-          {imgSrc: '../static/image/缺陷类型/视压.jpg', name: '视窗区压伤', details: '合格标准：\n长度<10mm,允许1条,程度按签样判定.\n'},
-          {imgSrc: '../static/image/缺陷类型/牙.jpg', name: '牙边', details: '合格标准：\n视窗区牙边：高度≦0.05mm，或单个牙缺≦0.02mm²\nAR孔 & PROX牙边：高度≦0.03mm.\n'},
-          {imgSrc: '../static/image/缺陷类型/油划.jpg', name: '油墨区划伤', details: '合格标准：\n≦0.01mm²,允许1个'},//TODO
-          {imgSrc: '../static/image/缺陷类型/油压.jpg', name: '油墨区压伤', details: '合格标准：\n≦0.08mm²,允许2个, 间距大于5mm.'},
-          {imgSrc: '../static/image/缺陷类型/油压痕.jpg', name: '油墨区压痕', details: '合格标准：\n长度<10mm,允许1条,程度按签样判定.'},
+          {imgSrc: '../static/image/缺陷类型/崩边.jpg', name: '崩边', details: '合格标准：\n≦0.02mm²,允许2个，\n或≦0.03mm²,允许1个', value: 1},
+          {imgSrc: '../static/image/缺陷类型/亮点.jpg', name: '亮点', details: '合格标准：\n≦0.01mm²,允许1个' , value: 2},
+          {imgSrc: '../static/image/缺陷类型/内污.jpg', name: '内污', details: '合格标准：\n白色:≦0.02mm²,允许2个,间距大于 5mm.\n黑色:≦0.05mm².' , value: 3},
+          {imgSrc: '../static/image/缺陷类型/缺墨.jpg', name: '缺墨', details: '合格标准：\n接收孔/菜单孔缺墨，透光：高度≦0.03mm,点状按≦0.03mm²,允许1个\n边缘缺墨：≦0.03mm²，允许一个' , value: 4},
+          {imgSrc: '../static/image/缺陷类型/视划.jpg', name: '视窗区划伤', details: '合格标准：\n长度<10mm,允许5条，不可交叉，明显度依签样；\n不可有视窗区反面垂直于短边划伤\n' , value: 5},
+          {imgSrc: '../static/image/缺陷类型/视压.jpg', name: '视窗区压伤', details: '合格标准：\n长度<10mm,允许1条,程度按签样判定.\n' , value: 6},
+          {imgSrc: '../static/image/缺陷类型/牙.jpg', name: '牙边', details: '合格标准：\n视窗区牙边：高度≦0.05mm，或单个牙缺≦0.02mm²\nAR孔 & PROX牙边：高度≦0.03mm.\n' , value: 7},
+          {imgSrc: '../static/image/缺陷类型/油划.jpg', name: '油墨区划伤', details: '合格标准：\n≦0.01mm²,允许1个' , value: 8},//TODO
+          {imgSrc: '../static/image/缺陷类型/油压.jpg', name: '油墨区压伤', details: '合格标准：\n≦0.08mm²,允许2个, 间距大于5mm.' , value: 0},
+          {imgSrc: '../static/image/缺陷类型/油压痕.jpg', name: '油墨区压痕', details: '合格标准：\n长度<10mm,允许1条,程度按签样判定.' , value: 10},
         ]
       }
     },
 
     methods: {
-      insurance(id) {
+      insurance(id, value) {
         this.$router.push({
           path: '/components/DefectDetectionPage',
           query: {
-            id: id
+            id: id,
+  value: value
           }
         })
       }
--- src/components/TrainingPage.vue
+++ src/components/TrainingPage.vue
@@ -1,9 +1,10 @@
 <template>
   <div style="padding-top:30px;padding-left: 15%">
-    <el-tabs :tab-position="tabPosition">
+    <el-tabs :tab-position="tabPosition" @tab-click="handleClick">
 <!--      tab1:新建训练文件-->
       <el-tab-pane label="新建训练" class="item-color">
-        <div style="padding-left:5%;padding-right: 10%">
+<!-- trainStatus为200，可以训练-->
+        <div style="padding-left:5%;padding-right: 10%"  v-if="trainStatus === 200">
           <el-form>
             <el-form-item>
               <span style="font-size: x-large;color: black;float:left">上传有缺陷图片</span>
@@ -54,10 +55,44 @@
 
           </el-form>
         </div>
+<!-- trainStatus为400，不可以训练-->
+<div style="padding-left:5%;padding-right: 10%;height: 300px"  v-else>
+<span>请稍候...</span>
+</div>
       </el-tab-pane>
 
 <!--tab2：已有的训练文件-->
-      <el-tab-pane label="训练文件"></el-tab-pane>
+      <el-tab-pane label="训练文件">
+<!--下拉框 -->
+<el-select v-model="value" placeholder="请选择" @change="function2(value)">
+<el-option
+  v-for="(item,index) in files"
+  :key="index"
+  :value="item"
+  :label="item">
+<!--<span style="float: left">{{ item }}</span>-->
+<!-- <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span> -->
+</el-option>
+</el-select>
+<!-- 上传图片  -->
+<div style="margin-top:20px">
+<el-upload
+class="upload-demo" name="photo"
+  ref="upload"
+  action="http://47.98.232.219:5000/pic_test"
+  :on-preview="handlePreview"
+  :on-remove="handleRemove"
+  :file-list="fileList"
+  list-type="picture"
+  :auto-upload="false"
+  :on-success="handleAvatarSuccess"
+  :before-upload="beforeUpload"
+  >
+<el-button slot="trigger" size="small" type="primary" :disabled="status ==-1?true:false">选取文件</el-button>
+<el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+</el-upload>
+</div>
+</el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -73,8 +108,19 @@
         tabPosition:'left',
         fileList1: [],
         fileList2: [],
+  value: '',
+  fileList: [ ],
+  files: [ ],
+  status: -1,//-1为禁用状态
+  trainStatus: '',
+  lable:'',
+  value:'',
+  key:'',
       }
     },
+  mounted () {
+  this.getTrain();
+  },
     methods: {
       handleChange(file, fileList) {
         this.fileList = fileList.slice(-3);
@@ -90,23 +136,70 @@
         this.$refs.upload.submit();
       },
 
+  //图片上传
+  submitUpload() {
+  this.$refs.upload.submit();
+  },
       //图片上传成功后
       handleAvatarSuccess(response, file, fileList) {
         //response
         console.log(response);
       },
+
+  handleRemove(file, fileList) {
+  console.log(file, fileList);
+  },
+
+  handlePreview(file) {
+  console.log(file);
+  },
+  beforeUpload(file) {
+
+  },
+  //判断可否训练
+  getTrain(){
+  this.$axios
+  .get('http://47.98.232.219:5000/to_train')
+  .then(response => {
+  console.log(response);
+  this.trainStatus = response.status;
+  console.log(this.trainStatus);
+  })
+  .catch(function (error) { // 请求失败处理
+  console.log(error);
+  });
+  },
+  getWeights(){
+  this.$axios
+  .get('http://47.98.232.219:5000/get_weights')
+  .then(response => {
+  console.log(response);
+  this.files = response.data.data;
+  })
+  .catch(function (error) { // 请求失败处理
+  console.log(error);
+  });
+  },
+  handleClick(tab, event){
+        if(tab.label == '训练文件'){
+          this.getWeights();
+  }
+  },
+  function2(value){
+        console.log(value);
+  }
     }
   }
 </script>
 
 
 <style>
-  .el-tabs_item{
-    background-color:#cccccc !important;
-    color:#666666 !important;
-    margin: 5px !important;
-  }
-  .el-tabs--card>.el-tabs_header.el-tabs_item.is-active{
+  .el-tabs__item.is-active{
     color:rgb(73,178,82) !important;
   }
+
+    .el-tabs__active-bar{
+    background-color:rgb(73,178,82);
+    }
+
 </style>
