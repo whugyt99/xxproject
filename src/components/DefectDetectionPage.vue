1a6cb9b4cc9ccc4857f5f9e5785d6e2dac653402
chenyuru <1244535094@qq.com>
2020-07-12 15:01:01 +0800

    feat:新建训练页面接口连接

--- src/components/DefectDetectionPage.vue
+++ src/components/DefectDetectionPage.vue
@@ -1,99 +1,99 @@
 <template>
-<div>
-<div style="margin-left:170px;margin-right:170px;margin-top:20px">
-<span>{{id}}缺陷检测</span>
-</div>
-<div style="margin-left:170px;margin-right:170px;margin-top:20px">
-<el-upload
-class="upload-demo" name="photo"
-  ref="upload"
-  :action="UploadUrl()"
-  :on-preview="handlePreview"
-  :on-remove="handleRemove"
-  :file-list="fileList"
-  list-type="picture"
-  :auto-upload="false"
-  :on-success="handleAvatarSuccess"
-  :before-upload="beforeUpload"
-  >
-<el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-<el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
-</el-upload>
-</div>
-</div>
+  <div>
+    <div style="margin-left:170px;margin-right:170px;margin-top:20px">
+      <span>{{id}}缺陷检测</span>
+    </div>
+    <div style="margin-left:170px;margin-right:170px;margin-top:20px">
+      <el-upload
+        class="upload-demo" name="photo"
+        ref="upload"
+        :action="UploadUrl()"
+        :on-preview="handlePreview"
+        :on-remove="handleRemove"
+        :file-list="fileList"
+        list-type="picture"
+        :auto-upload="false"
+        :on-success="handleAvatarSuccess"
+        :before-upload="beforeUpload"
+      >
+        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+        <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+      </el-upload>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-  name: 'DefectDetectionPage',
-  data () {
-  return {
-id:'',
-  value:'',
-  fileList: [ ],
-  pic_class:'',
-  url:'',
-  }
-  },
-  mounted () {
-  this.getParams();
-  //this.UploadUrl();
-  },
-  methods: {
-  getParams(){
-  // 取到路由带过来的参数
-  this.id = this.$route.query.id;
-  console.log(this.id);
-  this.value = this.$route.query.value;
-  console.log(this.value);
-  },
-  handleRemove(file, fileList) {
-  console.log(file, fileList);
-  },
+  export default {
+    name: 'DefectDetectionPage',
+    data() {
+      return {
+        id: '',
+        value: '',
+        fileList: [],
+        pic_class: '',
+        url: '',
+      }
+    },
+    mounted() {
+      this.getParams();
+      //this.UploadUrl();
+    },
+    methods: {
+      getParams() {
+        // 取到路由带过来的参数
+        this.id = this.$route.query.id;
+        console.log(this.id);
+        this.value = this.$route.query.value;
+        console.log(this.value);
+      },
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
 
-  handlePreview(file) {
-  console.log(file);
-  },
-  //图片上传
-  submitUpload() {
-    //let _this = this;
+      handlePreview(file) {
+        console.log(file);
+      },
+      //图片上传
+      submitUpload() {
+        //let _this = this;
 //setTimeout(function() {
-  //_this.$refs.upload.submit();
-  //},400)
-  this.$refs.upload.submit();
-  },
-  //图片上传成功后
-  handleAvatarSuccess(response, file, fileList) {
-  //response
-  console.log(response);
-  },
-  beforeUpload(file) {
-    return new Promise((resolve, reject) => {
-  //let _URL = window.URL || window.webkitURL;
-  let isLt2M = file.size / 1024 / 1024 < 2; // 判定图片大小是否小于2MB
-  if (isLt2M) {
-  resolve(file)
-  } else {
-  console.log('111');
-  const imageConversion = require("image-conversion");
-  //const es6promise = require('es6-promise').polyfill();
-  //console.log(file); // 压缩到400KB,这里的400就是要压缩的大小,可自定义
-  imageConversion.compressAccurately(file, 2048).then(res => {
-    console.log(res);
-  console.log('222');
-  resolve(res);
-  }).catch((error) => {
-  reject(error);
-  //throw new Error(error);
-  //console.log(error);
-  })
-  }
-  //img.src = _URL.createObjectURL(file);
-  })
-  },
-  UploadUrl:function(){
-  return "http://47.98.232.219:5000/upload_pic?pic_class="+this.value;
-  }
-  }
+        //_this.$refs.upload.submit();
+        //},400)
+        this.$refs.upload.submit();
+      },
+      //图片上传成功后
+      handleAvatarSuccess(response, file, fileList) {
+        //response
+        console.log(response);
+      },
+      beforeUpload(file) {
+        return new Promise((resolve, reject) => {
+          //let _URL = window.URL || window.webkitURL;
+          let isLt2M = file.size / 1024 / 1024 < 2; // 判定图片大小是否小于2MB
+          if (isLt2M) {
+            resolve(file)
+          } else {
+            console.log('111');
+            const imageConversion = require("image-conversion");
+            //const es6promise = require('es6-promise').polyfill();
+            //console.log(file); // 压缩到400KB,这里的400就是要压缩的大小,可自定义
+            imageConversion.compressAccurately(file, 2048).then(res => {
+              console.log(res);
+              console.log('222');
+              resolve(res);
+            }).catch((error) => {
+              reject(error);
+              //throw new Error(error);
+              //console.log(error);
+            })
+          }
+          //img.src = _URL.createObjectURL(file);
+        })
+      },
+      UploadUrl: function () {
+        return "http://47.98.232.219:5000/upload_pic?pic_class=" + this.value;
+      }
+    }
   }
 </script>
 <style>
--- src/components/TrainingPage.vue
+++ src/components/TrainingPage.vue
@@ -1,10 +1,10 @@
 <template>
   <div style="padding-top:30px;padding-left: 15%">
     <el-tabs :tab-position="tabPosition" @tab-click="handleClick">
-<!--      tab1:新建训练文件-->
+      <!--      tab1:新建训练文件-->
       <el-tab-pane label="新建训练" class="item-color">
-<!-- trainStatus为200，可以训练-->
-        <div style="padding-left:5%;padding-right: 10%"  v-if="trainStatus === 200">
+        <!-- trainStatus为200，可以训练-->
+        <div style="padding-left:5%;padding-right: 10%" v-if="trainStatus === 200">
           <el-form>
             <el-form-item>
               <span style="font-size: x-large;color: black;float:left">上传有缺陷图片</span>
@@ -14,7 +14,7 @@
               <el-upload
                 ref="upload"
                 class="upload-demo"
-                action="http://47.98.232.219:5000/pic_test"
+                action="http://47.98.232.219:5000/data_upload?pic_type=0"
                 list-type="picture"
                 :auto-upload="false"
                 multiple
@@ -27,7 +27,9 @@
               </el-upload>
             </el-form-item>
 
-            <el-form-item><el-divider></el-divider></el-form-item>
+            <el-form-item>
+              <el-divider></el-divider>
+            </el-form-item>
 
             <el-form-item>
               <span style="font-size: x-large;color: black;float:left">上传无缺陷图片</span>
@@ -35,11 +37,12 @@
 
             <el-form-item>
               <el-upload
+                ref="upload"
                 class="upload-demo"
                 multiple
                 :auto-upload="false"
                 list-type="picture"
-                action="http://47.98.232.219:5000/pic_test"
+                action="http://47.98.232.219:5000/data_upload?pic_type=1"
                 :on-change="handleChange"
                 :on-success="handleAvatarSuccess"
                 :file-list="fileList2">
@@ -55,44 +58,44 @@
 
           </el-form>
         </div>
-<!-- trainStatus为400，不可以训练-->
-<div style="padding-left:5%;padding-right: 10%;height: 300px"  v-else>
-<span>请稍候...</span>
-</div>
+        <!-- trainStatus为400，不可以训练-->
+        <div style="padding-left:5%;padding-right: 10%;height: 300px" v-else>
+          <span>请稍候...</span>
+        </div>
       </el-tab-pane>
 
-<!--tab2：已有的训练文件-->
+      <!--tab2：已有的训练文件-->
       <el-tab-pane label="训练文件">
-<!--下拉框 -->
-<el-select v-model="value" placeholder="请选择" @change="function2(value)">
-<el-option
-  v-for="(item,index) in files"
-  :key="index"
-  :value="item"
-  :label="item">
-<!--<span style="float: left">{{ item }}</span>-->
-<!-- <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span> -->
-</el-option>
-</el-select>
-<!-- 上传图片  -->
-<div style="margin-top:20px">
-<el-upload
-class="upload-demo" name="photo"
-  ref="upload"
-  action="http://47.98.232.219:5000/pic_test"
-  :on-preview="handlePreview"
-  :on-remove="handleRemove"
-  :file-list="fileList"
-  list-type="picture"
-  :auto-upload="false"
-  :on-success="handleAvatarSuccess"
-  :before-upload="beforeUpload"
-  >
-<el-button slot="trigger" size="small" type="primary" :disabled="status ==-1?true:false">选取文件</el-button>
-<el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
-</el-upload>
-</div>
-</el-tab-pane>
+        <!--下拉框 -->
+        <el-select v-model="value" placeholder="请选择" @change="function2(value)">
+          <el-option
+            v-for="(item,index) in files"
+            :key="index"
+            :value="item"
+            :label="item">
+            <!--<span style="float: left">{{ item }}</span>-->
+            <!-- <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span> -->
+          </el-option>
+        </el-select>
+        <!-- 上传图片  -->
+        <div style="margin-top:20px">
+          <el-upload
+            class="upload-demo" name="photo"
+            ref="upload"
+            action="http://47.98.232.219:5000/pic_test"
+            :on-preview="handlePreview"
+            :on-remove="handleRemove"
+            :file-list="fileList"
+            list-type="picture"
+            :auto-upload="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeUpload"
+          >
+            <el-button slot="trigger" size="small" type="primary" :disabled="status === -1">选取文件</el-button>
+            <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+          </el-upload>
+        </div>
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -100,106 +103,109 @@ class="upload-demo" name="photo"
 
 <script>
   import HelloWorld from "./HelloWorld";
+
   export default {
     name: 'TrainingPage',
     components: {HelloWorld},
     data() {
       return {
-        tabPosition:'left',
+        tabPosition: 'left',
         fileList1: [],
         fileList2: [],
-  value: '',
-  fileList: [ ],
-  files: [ ],
-  status: -1,//-1为禁用状态
-  trainStatus: '',
-  lable:'',
-  value:'',
-  key:'',
+        value: '',
+        fileList: [],
+        files: [],
+        status: -1,//-1为禁用状态
+        trainStatus: '',
+        label: '',
+        // value: '',
+        key: '',
       }
     },
-  mounted () {
-  this.getTrain();
-  },
+    mounted() {
+      this.getTrain();
+    },
     methods: {
       handleChange(file, fileList) {
         this.fileList = fileList.slice(-3);
       },
 
       //有缺陷图片上传
-      upLoad1(){
+      upLoad1() {
         this.$refs.upload.submit();
       },
 
       //无缺陷图片上传
-      upLoad2(){
+      upLoad2() {
         this.$refs.upload.submit();
       },
 
-  //图片上传
-  submitUpload() {
-  this.$refs.upload.submit();
-  },
+      //上传训练文件
+      submitUpload() {
+        this.$refs.upload.submit();
+      },
       //图片上传成功后
       handleAvatarSuccess(response, file, fileList) {
         //response
         console.log(response);
       },
 
-  handleRemove(file, fileList) {
-  console.log(file, fileList);
-  },
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
 
-  handlePreview(file) {
-  console.log(file);
-  },
-  beforeUpload(file) {
+      handlePreview(file) {
+        console.log(file);
+      },
+      beforeUpload(file) {
 
-  },
-  //判断可否训练
-  getTrain(){
-  this.$axios
-  .get('http://47.98.232.219:5000/to_train')
-  .then(response => {
-  console.log(response);
-  this.trainStatus = response.status;
-  console.log(this.trainStatus);
-  })
-  .catch(function (error) { // 请求失败处理
-  console.log(error);
-  });
-  },
-  getWeights(){
-  this.$axios
-  .get('http://47.98.232.219:5000/get_weights')
-  .then(response => {
-  console.log(response);
-  this.files = response.data.data;
-  })
-  .catch(function (error) { // 请求失败处理
-  console.log(error);
-  });
-  },
-  handleClick(tab, event){
-        if(tab.label == '训练文件'){
+      },
+      //判断可否训练
+      getTrain() {
+        this.$axios
+          .get('http://47.98.232.219:5000/to_train')
+          .then(response => {
+            console.log(response);
+            this.trainStatus = response.status;
+            console.log(this.trainStatus);
+          })
+          .catch(function (error) { // 请求失败处理
+            console.log(error);
+          });
+      },
+      getWeights() {
+        this.$axios
+          .get('http://47.98.232.219:5000/get_weights')
+          .then(response => {
+            console.log(response);
+            this.files = response.data.data;
+          })
+          .catch(function (error) { // 请求失败处理
+            console.log(error);
+          });
+      },
+      handleClick(tab, event) {
+        if (tab.label === '训练文件') {
           this.getWeights();
-  }
-  },
-  function2(value){
+        }
+      },
+      function2(value) {
         console.log(value);
-  }
+      }
     }
   }
 </script>
 
 
 <style>
-  .el-tabs__item.is-active{
-    color:rgb(73,178,82) !important;
+  .el-tabs__item.is-active {
+    color: rgb(73, 178, 82) !important;
   }
-
-    .el-tabs__active-bar{
-    background-color:rgb(73,178,82);
-    }
+  .el-tabs__active-bar {
+    background-color: rgb(73, 178, 82);
+  }
+  .el-tabs__item:hover {
+    color: rgb(73, 178, 82) !important;
+  }
 
 </style>
